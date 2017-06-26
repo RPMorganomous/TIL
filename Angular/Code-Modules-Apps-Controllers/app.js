3a8c22a8b37ef2007a6160e451559d0905a8f97c
Richard Morgan <mba.rick@gmail.com>
2017-06-25 19:14:36 -0500

    feat: new notes from today
    Reviewed bunch of stuff today.

---
+++ Angular/Code-Modules-Apps-Controllers/app.js
@@ -0,0 +1,11 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', function ($scope) {
+    
+console.log($scope);
+    <h1>Hello world!</h1>
+    
+}]);
+
