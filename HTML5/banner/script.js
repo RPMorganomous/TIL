aa44e6c8d583585699ea8b93bdc7ffd02b790925
Richard Morgan <mba.rick@gmail.com>
2017-07-27 07:55:47 -0500

    Section 11 complete

--- HTML5/banner/script.js
+++ HTML5/banner/script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    var banner = document.querySelector("main");
+    var logo = document.querySelector("#logo");
+    // var ud = document.querySelector("upsidedown");
+    setTimeout(function() {
+        var banner = document.querySelector("main");
+        banner.style.display 
+        banner.className = "visible";
+    }, 300);
+
+    banner.addEventListener("mouseover", function(){
+        banner.className = "over";
+        // logo.className = "upsidedown";
+    });
+    banner.addEventListener("mouseout", function(){
+        banner.className = "visible";
+    });
+    logo.addEventListener("mouseover", function(){
+        logo.className = "overLogo";
+    });
+    logo.addEventListener("mouseout", function(){
+        logo.className = "visible";
+    });
+    // ud.addEventListener("mouseover", function(){
+    //     ud.className = "overLogo";
+    // });
 })
